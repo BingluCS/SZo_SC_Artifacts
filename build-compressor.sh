7475644e1e919e5eb99a79bcf8fafda038fa10f6
BingLu <lu2020cjc@gmail.com>
2026-04-17 16:18:09 +0800

    init

---
+++ build-compressor.sh
@@ -0,0 +1,141 @@
+#!/usr/bin/env bash
+
+#set -e
+
+TARGET="$1"
+
+
+if [ ! -z "$2" ]; then
+    CPU_ARCH="$2"
+fi
+
+
+get_cpu_flag() {
+    local lib="$1"
+    local arch="$2"
+
+    case "$lib" in
+        SZo)
+            case "$arch" in
+                AVX2|avx2)
+                    echo "-DENABLE_AVX2=ON"
+                    ;;
+                SVE2|sve2)
+                    echo "-DENABLE_SVE2=ON"
+                    ;;
+                *)
+                    echo "-DENABLE_AVX2=OFF -DENABLE_SVE2=OFF"
+                    ;;
+            esac
+            ;;
+        SPERR)
+            case "$arch" in
+                AVX2|avx2)
+                    echo "-DENABLE_AVX2=ON"
+                    ;;
+                *)
+                    echo "-DENABLE_AVX2=OFF"
+                    ;;
+            esac
+            ;;
+        *)
+            echo ""
+            ;;
+    esac
+}
+
+build_PFPL() {
+    echo "installing PFPL..."
+    cd PFPL
+    make
+    cd ..
+}
+
+build_zfp() {
+    echo "installing zfp..."
+    cd zfp
+    make ZFP_WITH_OPENMP=ON
+    cd ..
+}
+
+build_SZ3() {
+    echo "installing SZ3..."
+    cd SZ3
+    mkdir build 
+    cd build
+    cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$(pwd)
+    make install -j
+    cd ../..
+}
+
+build_SZo() {
+    echo "installing SZo..."
+    cd SZo
+    mkdir build
+    cd build
+    CPU_FLAG=$(get_cpu_flag "SZo" "$CPU_ARCH")
+    cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$(pwd) $CPU_FLAG #-DENABLE_AVX2=ON
+    make install -j
+    cd ../..
+}
+
+build_SPERR() {
+    echo "installing SPERR..."
+    cd SPERR
+    mkdir build
+    cd build
+    CPU_FLAG=$(get_cpu_flag "SPERR" "$CPU_ARCH")
+    cmake ..  -DUSE_OMP=ON  $CPU_FLAG #-DENABLE_AVX2=ON
+    make -j
+    cd ../..
+}
+
+# build_tthresh() {
+#     echo "installing MGARD..."
+#     cd tthresh
+#     mkdir build
+#     cd build
+#      cmake -DCMAKE_BUILD_TYPE=Release ..
+#     cd ../..
+# }
+
+# build_tthresh() {
+#     echo "installing tthresh..."
+#     cd tthresh
+#     mkdir build
+#     cd build
+#      cmake -DCMAKE_BUILD_TYPE=Release ..
+#     cd ../..
+# }
+case "$TARGET" in
+    PFPL|pfpl)
+        build_PFPL
+        ;;
+    zfp|ZFP)
+        build_zfp
+        ;;
+    SZ3)
+	build_SZ3
+	;;
+    SZo)
+	build_SZo
+	;;
+    SPERR)
+	build_SPERR
+	;;
+    # tthresh)
+	# build_tthresh
+	;;
+    all)
+        build_PFPL
+        build_zfp
+        build_SZ3
+        build_SZo
+        build_SPERR
+        # build_tthresh
+        ;;
+    *)
+        echo "Usage: $0 {PFPL|pfpl|zfp|ZFP|SZ3|SZo|SPERR|MGARD|all}"
+#        exit 1
+        ;;
+esac
